f93826f571a5bf7c24219c639823b5f6fa31f5e4
CarpemSarah <saera1203@gmail.com>
2022-05-24 04:29:02 +0900

    map, mypage 수정

--- webapp/src/components/Chat/readChatDetail/TextInput.js
+++ webapp/src/components/Chat/readChatDetail/TextInput.js
@@ -1,84 +1,85 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import { createStyles, makeStyles } from "@mui/styles";
-import SendSharpIcon from '@mui/icons-material/SendSharp';
-import { Button, TextField } from '@mui/material';
-import { getValue } from '@testing-library/user-event/dist/utils';
-import { MessageLeft, MessageRight } from "components/Chat/readChatDetail/Message"
-import $ from "jquery";
+// import React, { useState } from 'react';
+// import ReactDOM from 'react-dom';
+// import { createStyles, makeStyles } from "@mui/styles";
+// import SendSharpIcon from '@mui/icons-material/SendSharp';
+// import { Button, TextField } from '@mui/material';
+// import { getValue } from '@testing-library/user-event/dist/utils';
+// import { MessageLeft, MessageRight } from "components/Chat/readChatDetail/Message"
+// import $ from "jquery";
+// import Api from "API/Api";
 
-var stompClient = null;
+// var stompClient = null;
 
-function sendName() {
-    stompClient.send(api + "/broadcast", {}, JSON.stringify({'name': $("#name").val()}));
-}
+// function sendName() {
+//     stompClient.send(api + "/broadcast", {}, JSON.stringify({'name': $("#name").val()}));
+// }
  
 
-function showGreeting(message) {
-    const rootElement = document.getElementById('style-1');
-            const elemet = React.createElement('div', {
-                children:
-                    <MessageRight message={message}></MessageRight>
-            })
-            ReactDOM.render(elemet, rootElement); 
-    /*
-    // if 내가 sender 라면?
-    const div = document.getElementById("style-1");
-    div.appendChild("<MessageRight>" + message + "</MessageRight>");
-    // else 남이 sender 라면?
-    div.appendChild("<MessageLeft>" + message + "</MessageLeft>");*/
-}
+// function showGreeting(message) {
+//     const rootElement = document.getElementById('style-1');
+//             const elemet = React.createElement('div', {
+//                 children:
+//                     <MessageRight message={message}></MessageRight>
+//             })
+//             ReactDOM.render(elemet, rootElement); 
+//     /*
+//     // if 내가 sender 라면?
+//     const div = document.getElementById("style-1");
+//     div.appendChild("<MessageRight>" + message + "</MessageRight>");
+//     // else 남이 sender 라면?
+//     div.appendChild("<MessageLeft>" + message + "</MessageLeft>");*/
+// }
 
 
-const useStyles = makeStyles((theme) =>
-    createStyles({
-        wrapForm: {
-            display: "flex",
-            justifyContent: "center",
-            width: "95%",
-            //margin: `${theme.spacing(0)} auto`
-        },
-        wrapText: {
-            width: "100%"
-        },
-        button: {
-            //margin: theme.spacing(1),
-        },
-    })
-);
+// const useStyles = makeStyles((theme) =>
+//     createStyles({
+//         wrapForm: {
+//             display: "flex",
+//             justifyContent: "center",
+//             width: "95%",
+//             //margin: `${theme.spacing(0)} auto`
+//         },
+//         wrapText: {
+//             width: "100%"
+//         },
+//         button: {
+//             //margin: theme.spacing(1),
+//         },
+//     })
+// );
 
 
 
 
-const TextInput = () => {
-    const classes = useStyles();
-    const [message, setMessage] = useState();
+// const TextInput = () => {
+//     const classes = useStyles();
+//     const [message, setMessage] = useState();
 
-    const postbody = {
-        roomId: "",
-        content: message,
-        sender: { id : "dsfsgs" }
-    };
-    return (
-        <>
-            <form className={classes.wrapForm} noValidate autoComplete="off">
-                <TextField
-                    id="standard-text"
-                    label=""
-                    className={classes.wrapText}
+//     const postbody = {
+//         roomId: "",
+//         content: message,
+//         sender: { id : "dsfsgs" }
+//     };
+//     return (
+//         <>
+//             <form className={classes.wrapForm} noValidate autoComplete="off">
+//                 <TextField
+//                     id="standard-text"
+//                     label=""
+//                     className={classes.wrapText}
 
-                    onChange={(event) => setMessage(event.target.value)}
-                    sx={{height: "100%"}}
-                    //margin="normal"
-                />
-                <Button variant="contained" color="primary" className={classes.button} 
-                      sx={{float: "bottom"}} >
-                    <SendSharpIcon />
-                </Button>
-            </form>
-        </>
-    )
-}
+//                     onChange={(event) => setMessage(event.target.value)}
+//                     sx={{height: "100%"}}
+//                     //margin="normal"
+//                 />
+//                 <Button variant="contained" color="primary" className={classes.button} 
+//                       sx={{float: "bottom"}} >
+//                     <SendSharpIcon />
+//                 </Button>
+//             </form>
+//         </>
+//     )
+// }
 
-export default TextInput
+// export default TextInput
 
--- webapp/src/components/Mypage/MypageLayout.js
+++ webapp/src/components/Mypage/MypageLayout.js
@@ -1,84 +1,127 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import styled from "styled-components";
+//import styled from "styled-components";
 import Header from 'components/Layout/Header/header';
 import Footer from 'components/Layout/Footer/footer';
-import MenuApp from 'components/Mypage/menu'
-import MyInfo from 'components/Mypage/myinfo'
-// import mainTheme from "./mainTheme";
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import MenuApp from 'components/Mypage/menu';
+import List from '@mui/material/List';
+import { mainListItems } from './menu2';
 
-const StyledLayout = styled.div`
-    padding-top: 80px;
-    margin: 0; /* margin default값으로 흰선 발생, 이를 제거 */
-    font-family: 'Source Sans Pro';
-`;
-const MainLayoutWrapper = styled.div`
-    display: flex;
-    flex: 1 1 auto;
-    overflow: hidden;
-    paddingTop: 64;
-`;
-const MainLayoutContainer = styled.div`
-    display: flex;
-    flex: 1 1 auto;
-    overflow: hidden;
-`;
-const StyledMain = styled.div`
-    display: flex;
-    min-height: calc(100vh - 180px);
-    margin:
-        margin-top: 0px;
-        margin-right: 210px;
-        margin-bottom: 0px;
-        margin-left: 210px;
-`;
-
-const StyledNav = styled.div`
-    display: block;
-    float: left;
-    width: 20%;
-    padding-top: 30px;
-    background-color: rgb(254, 248, 220);
-`;
-
-const StyledSection = styled.div`
-    display: block;
-    width: 80%;
-    padding-top: 50px;
-    padding-bottom: 50px;
-    padding-left: 50px;
-    font-size: 22px;
-    /*background-color: green;*/
-`;
-
-
-
-const StyledMypage = styled.div`
-    display: block;
-    margin-right: 50px;
-    font-size: 22px;
-    /*background-color: #ffca09;*/
-`;
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#ffca00',
+        },
+        secondary: {
+            main: '#ffb000',
+        },
+    },
+  });
 
 const MypageLayout = () => {
     const [isMobileNavOpen, setMobileNavOpen] = useState(false);
-
     return (
-        <StyledLayout>
-            <Header />
-                <StyledMain>
-                    <StyledNav>
-                        <MenuApp />
-                    </StyledNav>
-                    <StyledSection>
-                        <StyledMypage>
-                            <Outlet />
-                        </StyledMypage>
-                    </StyledSection>
-                </StyledMain>
-            <Footer />
-        </StyledLayout>
+        <ThemeProvider theme={theme}>
+            <Box sx={{ paddingTop: '80px'}} m={0} >
+                <Header />
+                <Container component="main" maxWidth="md"
+                    sx={{
+                        display: 'flex',
+                        flexGrow: 1,
+                        minHeight: 'calc(100vh - 180px)',
+                        overflow: 'auto',
+                    }}
+                >
+                    <Grid container >
+                        <Grid item lg={3} md={3} sx={{ display: { md: 'block', xs: 'none' }, backgroundColor: 'rgb(254, 248, 220)' }}>
+                            <List component="nav" sx={{ paddingTop: '30px' }}>
+                                {mainListItems}
+                            </List>
+                        </Grid>
+                        <Grid item lg={9} md={9} sm={12} xs={12} sx={{padding: '30px' }}>
+                            <Grid >
+                                <Outlet />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Container>
+                <Footer />
+            </Box>
+        </ThemeProvider>
     )
 }
 
-export default MypageLayout
+export default MypageLayout;
+
+// const StyledLayout = styled.div`
+//     padding-top: 80px;
+//     margin: 0; /* margin default값으로 흰선 발생, 이를 제거 */
+//     font-family: 'Source Sans Pro';
+// `;
+// const MainLayoutWrapper = styled.div`
+//     display: flex;
+//     flex: 1 1 auto;
+//     overflow: hidden;
+//     paddingTop: 64;
+// `;
+// const MainLayoutContainer = styled.div`
+//     display: flex;
+//     flex: 1 1 auto;
+//     overflow: hidden;
+// `;
+// const StyledMain = styled.div`
+//     display: flex;
+//     min-height: calc(100vh - 180px);
+//     margin:
+//         margin-top: 0px;
+//         margin-right: 210px;
+//         margin-bottom: 0px;
+//         margin-left: 210px;
+// `;
+
+// const StyledNav = styled.div`
+//     display: block;
+//     float: left;
+//     width: 20%;
+//     padding-top: 30px;
+//     background-color: rgb(254, 248, 220);
+// `;
+
+// const StyledSection = styled.div`
+//     display: block;
+//     width: 80%;
+//     padding-top: 50px;
+//     padding-bottom: 50px;
+//     padding-left: 50px;
+//     font-size: 22px;
+//     /*background-color: green;*/
+// `;
+
+
+
+// const StyledMypage = styled.div`
+//     display: block;
+//     margin-right: 50px;
+//     font-size: 22px;
+//     /*background-color: #ffca09;*/
+// `;
+    // return (
+    //     <StyledLayout>
+    //         <Header />
+    //             <StyledMain>
+    //                 <StyledNav>
+    //                     <MenuApp/>
+    //                 </StyledNav>
+    //                 <StyledSection>
+    //                     <StyledMypage>
+    //                         <Outlet />
+    //                     </StyledMypage>
+    //                 </StyledSection>
+    //             </StyledMain>
+    //         <Footer />
+    //     </StyledLayout>
+    // )
