14be994789e8752ce0664cb46337b2d30362c479
sojeongEom <esj5029@gmail.com>
2022-06-01 19:52:12 +0900

    base_url 변경

--- webapp/src/API/Url.js
+++ webapp/src/API/Url.js
@@ -1,4 +1,5 @@
 
-export const base_url = 'http://jueleejue.iptime.org:80';
+export const base_url = 'https://se337.duckdns.org:80';
+//export const base_url = 'http://jueleejue.iptime.org:80';
 //export const base_url = 'http://192.168.227.42:8080';
 
